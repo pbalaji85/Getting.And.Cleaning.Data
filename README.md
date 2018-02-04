52740f3aa2d4c83196441860b9f31d91bd2f31d6
Balaji Parameswaran <p.balaji@gmail.com>
2018-02-03 22:40:56 -0600

    Corrected Submission

--- README.md
+++ README.md
@@ -6,14 +6,23 @@ Repository](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Us
 The folder contains two sub-folders test and train. These two folders contain
 files with the X (feature) and Y (target) information.
 
-The script reads in the X (feature signals) and Y (target) from the training and
-test datasets, and then merges them into a single file.
+## Goals of the script
+* Merge the test and the training data sets to create one data set
+* Extract only the measurements on the mean and standard deviation for each measurement
+* Use descriptive activity names to name the activities in the data set
+* Appropriately label the data set with descriptive variable names
+* Create a second and independent tidy data set with the average of each
+variable for each activity and each subject
+
+The script reads in the X (feature signals) and Y (target), and Subject from the
+training and test datasets, and then merges them into a single DF.
 
 Further, we also add descriptive information about the activity (found in
-  activitylables.txt).
+activitylables.txt).
 
-The script also isolates the mean and standard deviation for the measurements
-and finally generates a tidy file that calculates the mean for each feature
+The script isolates features that are either mean and standard deviation of a
+measurement (Accelerometer, Gyroscope etc.).
+Finally, it generates a 'tidydata' file that calculates the mean for each feature
 after grouping by Activity and Subject.
 
-The tidy file has also uploaded as tidy.txt into this project.
+The tidy file has also uploaded as tidydata.txt into this project.
--- Run_analysis.R
+++ Run_analysis.R
@@ -15,20 +15,13 @@ feat <- rbind(x_train,x_test)
 ## Read in the file with all of the feature names
 feat_names <- read.table(file='features.txt')
 
+##################################################################################################
 ## Format the feature names to make them more readable - This will require multiple gsub calls
 feat_names$V2 <- gsub("()","",feat_names$V2,fixed=T)
 feat_names$V2 <- gsub("-","",feat_names$V2)
+feat_names$V2 <- gsub("BodyBody", "Body",feat_names$V2)
 
-## Now that we have formatted the feature names, let us ensure that they are are all unique values
-if (length(unique(feat_names$V2)!=length(feat_names$V2))) {
-  print("The feature names are not unique!")
-} else {
-  print("The feature names are all unique")
-}
-
-## It appears as if the feature names are non-unique. Let us rename the features with the generic name feat1 to feat561 
-## Assign the feature names to the features
-colnames(feat) <- paste("feat",seq(1:length(feat_names$V2)),sep = "")
+#################################################################################################
 
 ## Read in the target values for training set
 y_train <- read.table(file = 'train/y_train.txt')
@@ -41,6 +34,7 @@ target <- rbind(y_train,y_test)
 ## Add column names to the target dataset. The column names are abbreviates to 'target' for training feature
 colnames(target) <- "target"
 
+#######################################################################################
 ## Read in the information about Subject (both training and test)
 train_subject <- read.table(file = 'train/subject_train.txt')
 
@@ -51,27 +45,28 @@ subject <- rbind(train_subject,test_subject)
 colnames(subject) <- 'Subject'
 
 final <- cbind(feat,target,subject)
-
+colnames(final) <- feat_names$V2
 ## Extract the mean for each of the measurements (except the target variable)
 ExtractMeanSd <- grep('*.mean.*|*.std.*',feat_names$V2)
 ResultMeanSd <- final[,ExtractMeanSd]
+ResultMeanSd <- cbind(ResultMeanSd,Target=final$target,Subject=final$Subject)
 
 ## Convert final into a tibble
-final <- tbl_df(final)
+ResultMeanSd <- tbl_df(ResultMeanSd)
 
 ## Add descriptive activity labels for the activities in the dataset
 ## I'm using the mutate function in the dplyr package for this.
 
-final <- mutate(final, 
+ResultMeanSd <- mutate(ResultMeanSd, 
                 Activity = ifelse( target %in% 6, "Laying",ifelse( target == 5, "Standing", ifelse(target == 4, "Sitting", 
                       ifelse(target == 3, "Walking_Downstairs",ifelse(target == 2, "Walking_Upstairs", "Walking"))))))
 
 ## Group the dataframe by Activity and Subject
-final_grouped <- group_by(final, Activity, Subject)
+grouped <- group_by(ResultMeanSd, Activity, Subject)
 
 ## Calculate the mean for all the features grouped by Activity and Subject
-result <- summarize_all(final_grouped, funs(mean))
+result <- summarize_all(grouped, funs(mean))
 
 ## Finally export the table
-write.table(result,'tidy.txt',sep="\t", row.names = F)
+write.table(result,'tidydata.txt',sep="\t", row.names = F, quote = F)
                                                             
